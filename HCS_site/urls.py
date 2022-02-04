524f0f202b52446110b576a01fd7bbad290e9660
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-02-04 18:04:32 +0300

    04.02.2022 v3

--- HCS_site/urls.py
+++ HCS_site/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.index, name = "index" ),
     path('about-us/', views.about, name = "about"),
     path('news/', views.NewsListView.as_view(), name = "news_list"),
-    path('news-deatils/<int:pk>', views.NewsDetails.as_view(), name = "news_deatils"),
+    path('news/<int:pk>/', views.NewsDetails.as_view(), name = "news_deatils"),
     path('announcements/', views.AnnouncementListView.as_view(), name = "announcements_list"),
-    path('announcement/<int:pk>', views.AnnouncementDetails.as_view(), name = "announcement_details"),
+    path('announcements/<int:pk>/', views.AnnouncementDetails.as_view(), name = "announcement_details"),
     path('news/<int:pk>/', views.AddComment.as_view(), name = "add_comment"),
 ]
