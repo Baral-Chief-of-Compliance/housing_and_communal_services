8ae2161acd382095ad3ba1784d6656e31369ab5a
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-02-04 15:28:05 +0300

    04.02.2022

--- HCS_site/admin.py
+++ HCS_site/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from .models import News, Announcement, Complaint, Comments
 # Register your models here.
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ("address_of_complaint", "date_of_complaint",)
+    list_display_links = ("address_of_complaint",)
+    list_filter = ("address_of_complaint", "date_of_complaint",)
+
+
+class CommentsInline(admin.StackedInline):
+    model = Comments
+    extra = 1
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("name_of_news", "date_of_news",)
+    list_display_links = ("name_of_news",)
+    list_filter = ("date_of_news",)
+    search_fields = ("name_of_news",)
+    inlines = [CommentsInline]
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("name_of_commenter", "date_of_comment",)
+    readonly_fields = ("name_of_commenter",)
+    list_display_links = ("name_of_commenter", )
+    list_filter = ("date_of_comment",)
+
+
+admin.site.register(Announcement)
--- HCS_site/models.py
+++ HCS_site/models.py
@@ -5,14 +5,28 @@ from django.urls import reverse
 
 
 class News(models.Model):
-    name_of_news = models.CharField("Заголовок Новости", max_length=200)
+    name_of_news = models.CharField("Заголовок Новости", max_length = 200)
     date_of_news = models.DateField("Дата Новости", default = date.today)
     text_of_news = models.TextField("Текст Новости")
 
+    def __str__(self):
+        return self.name_of_news
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+
 
 class Announcement(models.Model):
-    name_of_announcement = models.CharField("Заголовок объявления", max_length=200)
+    name_of_announcement = models.CharField("Заголовок объявления", max_length = 200)
     image_of_announcement = models.ImageField("Изображение объявления", upload_to = 'HCS_site/')
+
+    def __str__(self):
+        return self.name_of_announcement
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
 
 
 class Complaint(models.Model):
@@ -20,8 +34,23 @@ class Complaint(models.Model):
     text_of_complaint = models.TextField("Текст заявки/жалобы")
     date_of_complaint = models.DateField("Дата заявки/жалобы", default = date.today)
 
+    def __str__(self):
+        return self.address_of_complaint
+
+    class Meta:
+        verbose_name = "Обращение"
+        verbose_name_plural = "Обращения"
+
 
 class Comments(models.Model):
+    news = models.ForeignKey(News, verbose_name = "Новось", on_delete = models.CASCADE )
     date_of_comment = models.DateField("Даты коментария", default = date.today)
-    name_of_commenter = models.CharField("Имя автора коменатрия", max_length=200)
-    text_of_comment = models.TextField("Текст коментария")
+    name_of_commenter = models.CharField("Имя автора коменатрия", max_length = 200)
+    text_of_comment = models.TextField("Текст коментария", max_length  = 5000)
+
+    def __str__(self):
+        return f"{self.name_of_commenter} - {self.news}"
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
